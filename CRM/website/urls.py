8f846158497f04fad10a3d5d309ab330ff675628
kleanti bashalli <58070317+kleanthis2001@users.noreply.github.com>
2023-09-29 14:14:27 +0300

    log in interface
    log in interface added

--- CRM/website/urls.py
+++ CRM/website/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home, name='home'),
-    path('login/',views.login_user, name='home'),
-    path('logout/',views.logout_user, name='home'),
+    # path('login/',views.login_user, name='home'),
+    # path('logout/',views.logout_user, name='home'),
 ]
